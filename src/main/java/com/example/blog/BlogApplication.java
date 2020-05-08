2e5957e17687617dbfe33790b58131e9ac13d0bd
ming <bangmingzhang@gmail.com>
2020-05-08 13:30:34 +0930

    set up basic frame for project using Spring and React

---
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.example.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ming
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
